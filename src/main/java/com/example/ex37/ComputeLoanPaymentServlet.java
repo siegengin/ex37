357650f4a6dc614f1ed331055428f87a5866e217
cassandra sherlock <siegengin@gmail.com>
2023-10-08 13:30:20 -0400

    Initial commit

---
+++ src/main/java/com/example/ex37/ComputeLoanPaymentServlet.java
@@ -0,0 +1,18 @@
+package com.example.ex37;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ComputerLoanPaymentServlet", value = "/ComputerLoanPaymentServlet")
+public class ComputerLoanPaymentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/com/example/ex37/Loan.java
@@ -0,0 +1,2 @@
+package com.example.ex37;public class Loan {
+}
